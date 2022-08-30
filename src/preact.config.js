c4981f9231efb4eae46eb8f6d84dcef2ff541499
Jet Fontanilla <jetlogs@gmail.com>
2022-08-30 11:09:18 +0900

    update base href

--- src/preact.config.js
+++ src/preact.config.js
@@ -5,8 +5,7 @@ export default (config, env, helpers) => {
     config.output.publicPath = '/' + (process.env.BASE_URL || "");
     config.plugins.push(
         new helpers.webpack.DefinePlugin({
-            PUBLIC_PATH: JSON.stringify(config.output.publicPath),
-            NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+            PUBLIC_PATH: JSON.stringify(config.output.publicPath)
         })
     );
 };
