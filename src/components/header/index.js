afcd4a592c17a309c61b7883a5d2f093a0c5665a
Jet Fontanilla <jetlogs@gmail.com>
2022-08-30 10:56:01 +0900

    Initial media tester code

---
+++ src/components/header/index.js
@@ -0,0 +1,11 @@
+import style from "./style.css";
+
+const Header = () => {
+    return (
+        <header class={style.header}>
+            <h1>Media Recorder Tester</h1>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/routes/home/index.js
@@ -0,0 +1,32 @@
+import style from "./style.css";
+
+const Home = () => {
+    const mediaRecorderSupported = !!window.MediaRecorder;
+    const mediaRecorderIsTypeSupported = !!MediaRecorder.isTypeSupported;
+    const isMp3Supported = mediaRecorderIsTypeSupported && MediaRecorder.isTypeSupported("audio/mp3");
+    const isMp4Supported = mediaRecorderIsTypeSupported && MediaRecorder.isTypeSupported("audio/mp4");
+    const isOpusWebmSupported = mediaRecorderIsTypeSupported && MediaRecorder.isTypeSupported("audio/webm;codecs=opus");
+    const isOpusOggSupported = mediaRecorderIsTypeSupported && MediaRecorder.isTypeSupported("audio/ogg;codecs=opus");
+
+    return (
+        <div class={style.home}>
+            <h1>Media Recorder Tester</h1>
+            <div class={style.row}>
+                <div>Media Recorder API</div>
+                <div>{mediaRecorderSupported ? "true" : "false"}</div>
+                <div>MediaRecorder.isTypeSupported</div>
+                <div>{mediaRecorderIsTypeSupported ? "true" : "false"}</div>
+                <div>mp3 support</div>
+                <div>{isMp3Supported ? "true" : "false"}</div>
+                <div>mp4 support</div>
+                <div>{isMp4Supported ? "true" : "false"}</div>
+                <div>opus/webm support</div>
+                <div>{isOpusWebmSupported ? "true" : "false"}</div>
+                <div>opus/ogg support</div>
+                <div>{isOpusOggSupported ? "true" : "false"}</div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
