bebe31cf2e29da7b709bbfa677849c831ac10c22
Jet Fontanilla <jetlogs@gmail.com>
2022-08-30 11:37:10 +0900

    add GitHub link

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,13 @@
 import style from "./style.css";
+import githubDark from "./github-dark.png";
 
 const Header = () => {
     return (
         <header class={style.header}>
             <h1>Media Recorder Tester</h1>
+            <a className="github" href="https://github.com/jetfontanilla/media-recorder-tester">
+                <img src={githubDark} width="32" height="32" alt="View on Github"/>
+            </a>
         </header>
     );
 };
